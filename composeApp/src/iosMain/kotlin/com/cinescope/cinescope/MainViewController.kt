628f22c01c61e49a7174d0d531d1d6aebd9007e2
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-12-20 09:56:15 +0100

    Initialize Koin DI in MainViewController for iOS

--- composeApp/src/iosMain/kotlin/com/cinescope/cinescope/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/cinescope/cinescope/MainViewController.kt
@@ -1,5 +1,18 @@
 package com.cinescope.cinescope
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.cinescope.cinescope.di.appModule
+import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    initKoin()
+    App()
+}
+
+private fun initKoin() {
+    if (org.koin.core.context.GlobalContext.getOrNull() == null) {
+        startKoin {
+            modules(appModule)
+        }
+    }
+}
