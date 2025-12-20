097e0beec5f6b248347052f1128bd578578bfeea
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-12-20 09:55:07 +0100

    Add Screen sealed class for defining app navigation routes

---
+++ composeApp/src/commonMain/kotlin/com/cinescope/cinescope/presentation/navigation/Screen.kt
@@ -0,0 +1,13 @@
+package com.cinescope.cinescope.presentation.navigation
+
+sealed class Screen(val route: String) {
+    data object Home : Screen("home")
+    data object Search : Screen("search")
+    data object Details : Screen("details/{tmdbId}/{contentType}") {
+        fun createRoute(tmdbId: Int, contentType: String = "movie") = "details/$tmdbId/$contentType"
+    }
+    data object Ratings : Screen("ratings")
+    data object Recommendations : Screen("recommendations")
+    data object Statistics : Screen("statistics")
+    data object Watchlist : Screen("watchlist")
+}
