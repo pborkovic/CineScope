0f18bd079e262cf25868226bfd1b467648f1c258
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-12-20 09:56:01 +0100

    Add CineScopeApplication for Koin DI initialization

--- composeApp/src/androidMain/kotlin/com/cinescope/cinescope/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/cinescope/cinescope/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.cinescope.cinescope
 
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.cinescope.cinescope.di.appModule
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.dsl.module
+
+class CineScopeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            modules(
+                module {
+                    single<Context> { this@CineScopeApplication }
+                },
+                appModule
+            )
+        }
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
