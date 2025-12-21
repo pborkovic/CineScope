cb823ca15da8601f174873c717714494c873357a
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-12-21 18:20:21 +0100

    Annotate Movie model with `@OptIn` for experimental Kotlin time functionality

--- composeApp/src/commonMain/kotlin/com/cinescope/cinescope/domain/model/Movie.kt
+++ composeApp/src/commonMain/kotlin/com/cinescope/cinescope/domain/model/Movie.kt
@@ -2,6 +2,7 @@ package com.cinescope.cinescope.domain.model
 
 import kotlinx.datetime.Instant
 
+@OptIn(kotlin.time.ExperimentalTime::class)
 /**
  * Represents a movie entity in the CineScope application.
  *
