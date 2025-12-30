3a18e18bc4977f9c5d06a87d6ac94335fd87fe93
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-12-30 21:55:48 +0100

    Refactor: Remove unnecessary `KoinContext` wrapper in `App` composable

--- composeApp/src/commonMain/kotlin/com/cinescope/cinescope/App.kt
+++ composeApp/src/commonMain/kotlin/com/cinescope/cinescope/App.kt
@@ -2,16 +2,13 @@ package com.cinescope.cinescope
 
 import androidx.compose.runtime.*
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.KoinContext
 import com.cinescope.cinescope.presentation.theme.CineScopeTheme
 import com.cinescope.cinescope.presentation.navigation.AppNavigation
 
 @Composable
 @Preview
 fun App() {
-    KoinContext {
-        CineScopeTheme {
-            AppNavigation()
-        }
+    CineScopeTheme {
+        AppNavigation()
     }
 }
