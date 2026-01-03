71de655be91c67c3f022bff64056f468c78ec889
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2026-01-03 18:07:50 +0100

    Add dynamic theme support based on user preferences

--- composeApp/src/commonMain/kotlin/com/cinescope/cinescope/App.kt
+++ composeApp/src/commonMain/kotlin/com/cinescope/cinescope/App.kt
@@ -1,14 +1,33 @@
 package com.cinescope.cinescope
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.*
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import com.cinescope.cinescope.domain.model.ThemePreference
+import com.cinescope.cinescope.domain.repository.UserProfileRepository
 import com.cinescope.cinescope.presentation.theme.CineScopeTheme
 import com.cinescope.cinescope.presentation.navigation.AppNavigation
+import org.koin.compose.koinInject
 
 @Composable
 @Preview
 fun App() {
-    CineScopeTheme {
+    val userProfileRepository: UserProfileRepository = koinInject()
+    var themePreference by remember { mutableStateOf(ThemePreference.SYSTEM) }
+
+    LaunchedEffect(Unit) {
+        userProfileRepository.getUserProfile().collect { profile ->
+            themePreference = profile.themePreference
+        }
+    }
+
+    val isDarkTheme = when (themePreference) {
+        ThemePreference.SYSTEM -> isSystemInDarkTheme()
+        ThemePreference.LIGHT -> false
+        ThemePreference.DARK -> true
+    }
+
+    CineScopeTheme(darkTheme = isDarkTheme) {
         AppNavigation()
     }
 }
